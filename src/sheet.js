fb7d1ed783b76873cfe66007adabf6cb871e52b5
Gen351 <genesisroner.lozada351@gmail.com>
2026-06-18 09:18:09 +0800

    Fix: idk man

--- src/sheet.js
+++ src/sheet.js
@@ -2,22 +2,14 @@ const SHEET_ID = "1-vZigfxDtew870csptOHzjw4JUKwPwuY9jEUW1GtqcM";
 const SHEET_URL = "https://docs.google.com/spreadsheets/d/1-vZigfxDtew870csptOHzjw4JUKwPwuY9jEUW1GtqcM"
 
 // Vite only exposes env vars prefixed with `VITE_` to client code.
-const SHEET_ENDPOINT = import.meta.env.VITE_SHEET_ENDPOINT;
-const APP_SECRET_KEY = import.meta.env.VITE_APP_SECRET_KEY;
+const SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbyftBH2a6IW0zzZAp8_GUbYexreSKUWOJrQzXR8x9qx9h3lMhwabqLerCPYrkP1dxny/exec";
 
 if (!SHEET_ENDPOINT) {
     console.warn('SHEET_ENDPOINT is not set. Make sure .env contains VITE_SHEET_ENDPOINT and restart the dev server.');
 }
-if (!APP_SECRET_KEY) {
-    console.warn('APP_SECRET_KEY is not set. Make sure .env contains VITE_APP_SECRET_KEY and restart the dev server.');
-}
 
 export function getSheetId() {
     return SHEET_ID;
-}
-
-export function getAppKey() {
-    return APP_SECRET_KEY;
 }
 
 export function getSheetEndpoint() {
